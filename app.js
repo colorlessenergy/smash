a4482a5cf6c1e235e21c1e309f66caf39b88355d
colorlessenergy <brianmunozdev@gmail.com>
2018-06-02 09:58:24 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-/*
-  currently trying to figure out how to display 4 empty boxes.
-  first load
-*/
-
 Vue.component("thumbnail", {
-  template: '<img :src="pic" v-on:click="$emit(\'chose\')" />',
+  template: '<img :src="pic" v-on:click="$emit(\'chose\')" v-on:mouseenter="$emit(\'picking\')" />',
   props: ["pic"]
 });
 
@@ -25,19 +20,32 @@ Vue.component("portrait", {
   props: ["pic", "name", "icon"]
 });
 
+Vue.component("select-char", {
+  template: '<img v-on:click="$emit(\'chose\', $event)" v-on:mousemove="$emit(\'follow\', $event)" :src="pick" />',
+  props: ["pick"]
+})
+
 
 var app = new Vue({
   el: "#app",
   data: {
     message: "Hello, world!",
     characters: [
-      {id: 0, name: "image/captainfalcon-name.png", thumbnail: "image/captain-thumbnail.png", portrait: "image/captain-portrait.png", pickSound: 'image/pick-cp.wav', icon: "image/icon-cp.png"},
+      {id: 0, name: "image/captain-falcon/captainfalcon-name.png", thumbnail: "image/captain-falcon/captain-thumbnail.png", portrait: "image/captain-falcon/captain-portrait.png", pickSound: 'image/captain-falcon/pick-cp.wav', icon: "image/captain-falcon/icon-cp.png"},
+      {id: 1, name: "image/dk/dk-name.png", thumbnail: "image/dk/dk-thumbnail.png", portrait: "image/dk/dk-portrait.png", pickSound: 'image/dk/dk_select.mp3', icon: "image/dk/dk-icon.png"},
+
     ],
     currentCharacter: [
-      {id: 0, name: "?", thumbnail: "", portrait: "?", pickSound: '', icon: ""},
-      {id: 0, name: "?", thumbnail: "", portrait: "?", pickSound: '', icon: ""},
-      {id: 0, name: "?", thumbnail: "", portrait: "?", pickSound: '', icon: ""},
-      {id: 0, name: "?", thumbnail: "", portrait: "?", pickSound: '', icon: ""}
+      {id: 0, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "lightblue"},
+      {id: 1, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "red"},
+      {id: 2, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "green"},
+      {id: 3, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "blue"}
+    ],
+    select: [
+      {id: 0, src: 'image/cursor/p1.png', active: false},
+      {id: 1, src: 'image/cursor/p2.png', active: false},
+      {id: 2, src: 'image/cursor/p3.png', active: false},
+      {id: 3, src: 'image/cursor/p4.png', active: false}
     ]
   },
 
@@ -53,6 +61,39 @@ var app = new Vue({
       var pickAudio = new Audio();
       pickAudio.src = this.currentCharacter[0].pickSound;
       pickAudio.play();
+    },
+
+    hoverCharacter: function (char, index){
+      this.currentCharacter[0].id = char.id;
+      this.currentCharacter[0].name = char.name;
+      this.currentCharacter[0].thumbnail = char.thumnail;
+      this.currentCharacter[0].portrait = char.portrait;
+      this.currentCharacter[0].icon = char.icon;
+    },
+
+    pickCharacter: function (index, event) {
+      var el = event.currentTarget;
+      this.move = function(e) {
+        // console.log(el, e)
+        el.style.left = (e.clientX - 30) + "px";
+        el.style.top = (e.clientY - 30) + "px";
+      }
+      var move = this.move;
+      var select = this.select;
+      this.select[index].active = true;
+      if (this.select[index].active) {
+        window.addEventListener("mousemove", function (e) {
+          move(e)
+        }, false);
+        el.addEventListener("click", function (ev){
+          ev.stopPropagation();
+          console.log("clicked")
+          select[index].active = false;
+          window.removeEventListener("mousemove", function (e) {
+            move(e)
+          }, false);
+        });
+      }
     }
   }
 })
