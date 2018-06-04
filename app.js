9cc2c814595cd1eee171af5e058218bf331f11fa
colorlessenergy <brianmunozdev@gmail.com>
2018-06-03 17:55:16 -0700

    fix bug where couldn't drop the selector on top of another selector and starting to implement skin selection

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// add event listners for the numbers 1 2 3 4
+Vue.config.keyCodes.one = 49;
+Vue.config.keyCodes.two = 50;
+Vue.config.keyCodes.three = 51;
+Vue.config.keyCodes.four = 52;
+
+
 Vue.component("thumbnail", {
   template: '<img :src="pic" v-on:click="$emit(\'chose\')" />',
   props: ["pic"]
@@ -31,8 +38,10 @@ var app = new Vue({
   data: {
     message: "Hello, world!",
     characters: [
-      {id: 0, name: "image/captain-falcon/captainfalcon-name.png", thumbnail: "image/captain-falcon/captain-thumbnail.png", portrait: "image/captain-falcon/captain-portrait.png", pickSound: 'image/captain-falcon/pick-cp.wav', icon: "image/captain-falcon/icon-cp.png"},
-      {id: 1, name: "image/dk/dk-name.png", thumbnail: "image/dk/dk-thumbnail.png", portrait: "image/dk/dk-portrait.png", pickSound: 'image/dk/dk_select.mp3', icon: "image/dk/dk-icon.png"},
+      {id: 0, name: "image/captain-falcon/captainfalcon-name.png", thumbnail: "image/captain-falcon/captain-thumbnail.png", portrait: "image/captain-falcon/captain-portrait.png", pickSound: 'image/captain-falcon/pick-cp.wav', icon: "image/captain-falcon/icon-cp.png",
+      skins: ["image/captain-falcon/alt/cp-black.png", "image/captain-falcon/alt/cp-red.png", "image/captain-falcon/alt/cp-white.png", "image/captain-falcon/captain-portrait.png"]},
+      {id: 1, name: "image/dk/dk-name.png", thumbnail: "image/dk/dk-thumbnail.png", portrait: "image/dk/dk-portrait.png", pickSound: 'image/dk/dk_select.mp3', icon: "image/dk/dk-icon.png",
+      skins: ["image/dk/alt/dk-black.png", "image/dk/alt/dk-pink.png", "image/dk/alt/dk-white.png", "image/dk/dk-portrait.png"]},
 
     ],
     currentCharacter: [
@@ -53,7 +62,13 @@ var app = new Vue({
       x: 0,
       y: 0
     },
-    index: 0
+    index: 0,
+    changeSkin: [
+      {currentSkin: 0, max: 4},
+      {currentSkin: 0, max: 4},
+      {currentSkin: 0, max: 4},
+      {currentSkin: 0, max: 4}
+    ],
   },
 
   methods: {
@@ -68,6 +83,7 @@ var app = new Vue({
       var pickAudio = new Audio();
       pickAudio.src = this.currentCharacter[this.index].pickSound;
       pickAudio.play();
+      this.index = char.id;
     },
 
     hoverCharacter: function (char, index){
@@ -93,45 +109,66 @@ var app = new Vue({
       // alert(index, event)
       var that = this;
       console.log(event, index)
+      var selectChar = document.querySelectorAll(".selectChar");
       if (this.currentDrag === false) {
+        // move elements from top layer to bottom
+        // initial pick of selectors
+        selectChar.forEach(function (ele) {
+          console.log(ele)
+          ele.style.zIndex = -3;
+        });
         this.currentDrag = true;
         this.currentElement = event.currentTarget;
+        that.currentElement.style.zIndex = 1;
+
         console.log(index, event, this.currentElement)
       } else {
         this.currentElement.style.zIndex = -1;
         this.currentDrag = false;
         window.setTimeout(function () {
-          // console.log(that.mouseP)
           that.index = index;
           document.elementFromPoint(that.mousePos.x, that.mousePos.y).click();
           that.currentElement.style.zIndex = 1;
           that.currentElement = false;
+          // move elements from bottom layer to top
+          // after dropping off the selector
+          selectChar.forEach(function (ele) {
+            ele.style.zIndex = 1;
+          });
         }, 1)
       }
-      // var el = event.currentTarget;
+    },
 
-      // var move = this.move;
-      // var select = this.select;
-      // this.select[index].active = true;
-      // if (this.select[index].active) {
-      //   window.addEventListener("mousemove", function (e) {
-      //     move(e)
-      //   }, false);
-        // document.querySelector(".thumbnail__display").addEventListener("click", function (ev){
-        //   ev.stopPropagation();
-        //   select[index].active = false;
-        //   window.removeEventListener("mousemove", function (e) {
-        //     move(e)
-        //   }, false);
-        // });
-      // }
+    changeCharSkin: function(index) {
+      if (this.changeSkin[index].currentSkin >= this.changeSkin[index].max) {
+        this.changeSkin[index].currentSkin = 0;
+      }
+      this.currentCharacter[index].portrait = this.characters[this.index].skins[this.changeSkin[index].currentSkin];
+      this.changeSkin[index].currentSkin++;
     }
   }, mounted() {
     var self = this;
     window.addEventListener('mousemove', function (event) {
       self.mousePos.x = event.clientX;
       self.mousePos.y = event.clientY;
+    });
 
+    window.addEventListener('keyup', function (event) {
+      switch (event.keyCode) {
+        case 49:
+        self.changeCharSkin(0);
+          break;
+        case 50:
+        self.changeCharSkin(1);
+          break;
+        case 51:
+        self.changeCharSkin(2);
+          break;
+        case 52:
+        self.changeCharSkin(3);
+          break;
+
+      }
     });
   }
 })
