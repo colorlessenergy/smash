a1188d650827b4496b72c4192e9bb32f8027e2a0
colorlessenergy <brianmunozdev@gmail.com>
2018-06-08 01:17:47 -0700

    update the way to select characters

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// add event listners for the numbers 1 2 3 4
-Vue.config.keyCodes.one = 49;
-Vue.config.keyCodes.two = 50;
-Vue.config.keyCodes.three = 51;
-Vue.config.keyCodes.four = 52;
-
-
 Vue.component("thumbnail", {
   template: '<img :src="pic" v-on:click="$emit(\'chose\')" />',
   props: ["pic"]
@@ -28,7 +21,7 @@ Vue.component("portrait", {
 });
 
 Vue.component("select-char", {
-  template: '<img v-on:click="$emit(\'chose\', $event)" v-on:mousemove="$emit(\'hi\')" :src="pick" />',
+  template: '<img v-on:click="$emit(\'chose\')" :src="pick" />',
   props: ["pick"]
 })
 
@@ -36,7 +29,6 @@ Vue.component("select-char", {
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello, world!",
     characters: [
       {id: 0, name: "image/captain-falcon/captainfalcon-name.png", thumbnail: "image/captain-falcon/captain-thumbnail.png", portrait: "image/captain-falcon/captain-portrait.png", pickSound: 'image/captain-falcon/pick-cp.wav', icon: "image/captain-falcon/icon-cp.png",
       skins: ["image/captain-falcon/alt/cp-black.png", "image/captain-falcon/alt/cp-red.png", "image/captain-falcon/alt/cp-white.png", "image/captain-falcon/captain-portrait.png"]},
@@ -45,24 +37,23 @@ var app = new Vue({
 
     ],
     currentCharacter: [
-      {id: 0, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "lightblue"},
-      {id: 1, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "red"},
-      {id: 2, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "green"},
-      {id: 3, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "blue"}
+      {id: 0, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "lightblue", skins: undefined},
+      {id: 1, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "red", skins: undefined},
+      {id: 2, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "green", skins: undefined},
+      {id: 3, name: "?", thumbnail: "image/question-mark/question-mark.png", portrait: "image/question-mark/question-mark.png", pickSound: '', icon: "image/question-mark/question-mark.png", color: "blue", skins: undefined}
     ],
     select: [
-      {id: 0, src: 'image/cursor/p1.png', active: false},
-      {id: 1, src: 'image/cursor/p2.png', active: false},
-      {id: 2, src: 'image/cursor/p3.png', active: false},
-      {id: 3, src: 'image/cursor/p4.png', active: false}
+      {id: 0, src: 'image/cursor/p1.png', active: false, color: 'lightblue'},
+      {id: 1, src: 'image/cursor/p2.png', active: false, color: 'red'},
+      {id: 2, src: 'image/cursor/p3.png', active: false, color: 'green'},
+      {id: 3, src: 'image/cursor/p4.png', active: false, color: 'blue'}
     ],
-    currentDrag: false,
-    currentElement: undefined,
+    currentDrag: true,
     mousePos: {
       x: 0,
       y: 0
     },
-    index: 0,
+    charSelectIndex: 0,
     changeSkin: [
       {currentSkin: 0, max: 4},
       {currentSkin: 0, max: 4},
@@ -73,69 +64,36 @@ var app = new Vue({
 
   methods: {
     addChar: function (char) {
-      this.currentCharacter[this.index].id = char.id;
-      this.currentCharacter[this.index].name = char.name;
-      this.currentCharacter[this.index].thumbnail = char.thumnail;
-      this.currentCharacter[this.index].portrait = char.portrait;
-      this.currentCharacter[this.index].pickSound = char.pickSound;
-      this.currentCharacter[this.index].icon = char.icon;
-
+      // this.charSelectIndex = index;
+      // this.currentCharacter[this.charSelectIndex].id = char.id;
+      this.currentCharacter[this.charSelectIndex].name = char.name;
+      this.currentCharacter[this.charSelectIndex].thumbnail = char.thumnail;
+      this.currentCharacter[this.charSelectIndex].portrait = char.portrait;
+      this.currentCharacter[this.charSelectIndex].pickSound = char.pickSound;
+      this.currentCharacter[this.charSelectIndex].icon = char.icon;
+      this.currentCharacter[this.charSelectIndex].skins = char.skins;
       var pickAudio = new Audio();
-      pickAudio.src = this.currentCharacter[this.index].pickSound;
+      pickAudio.src = this.currentCharacter[this.charSelectIndex].pickSound;
       pickAudio.play();
-      this.index = char.id;
-    },
 
-    hoverCharacter: function (char, index){
-      this.currentCharacter[0].id = char.id;
-      this.currentCharacter[0].name = char.name;
-      this.currentCharacter[0].thumbnail = char.thumnail;
-      this.currentCharacter[0].portrait = char.portrait;
-      this.currentCharacter[0].icon = char.icon;
-    },
-
-    move: function(e) {
-      console.log(this.currentElement);
-      if (this.currentElement) {
-        this.currentElement.style.left = (this.mousePos.x - 30) + "px";
-        this.currentElement.style.top = (this.mousePos.y - 30) + "px";
-      }
-      // if (this.currentDrag) {
-      //   this.currentElement.currentTarget.style.left = (this.mousePos.x.clientX - 30) + "px";
-      // }
-    },
-
-    pickCharacter: function (index, event) {
-      // alert(index, event)
-      var that = this;
-      console.log(event, index)
       var selectChar = document.querySelectorAll(".selectChar");
-      if (this.currentDrag === false) {
-        // move elements from top layer to bottom
-        // initial pick of selectors
-        selectChar.forEach(function (ele) {
-          console.log(ele)
-          ele.style.zIndex = -3;
-        });
-        this.currentDrag = true;
-        this.currentElement = event.currentTarget;
-        that.currentElement.style.zIndex = 1;
+      console.log(this.charSelectIndex)
 
-        console.log(index, event, this.currentElement)
-      } else {
-        this.currentElement.style.zIndex = -1;
+      document.querySelector("body").classList.toggle(this.select[this.charSelectIndex].color)
+      selectChar[this.charSelectIndex].style.display = 'block';
+      selectChar[this.charSelectIndex].style.left = (this.mousePos.x) + "px";
+      selectChar[this.charSelectIndex].style.top = (this.mousePos.y) + "px";
+      this.currentDrag = true;
+    },
+
+    pickCharacter: function (index) {
+      var selectChar = document.querySelectorAll(".selectChar");
+      if (this.currentDrag) {
+        this.charSelectIndex = index;
         this.currentDrag = false;
-        window.setTimeout(function () {
-          that.index = index;
-          document.elementFromPoint(that.mousePos.x, that.mousePos.y).click();
-          that.currentElement.style.zIndex = 1;
-          that.currentElement = false;
-          // move elements from bottom layer to top
-          // after dropping off the selector
-          selectChar.forEach(function (ele) {
-            ele.style.zIndex = 1;
-          });
-        }, 1)
+        console.log(this.currentDrag)
+        document.querySelector("body").classList.toggle(this.select[index].color)
+        selectChar[index].style.display = 'none';
       }
     },
 
@@ -143,10 +101,11 @@ var app = new Vue({
       if (this.changeSkin[index].currentSkin >= this.changeSkin[index].max) {
         this.changeSkin[index].currentSkin = 0;
       }
-      this.currentCharacter[index].portrait = this.characters[this.index].skins[this.changeSkin[index].currentSkin];
+      this.currentCharacter[index].portrait = this.currentCharacter[index].skins[this.changeSkin[index].currentSkin];
       this.changeSkin[index].currentSkin++;
     }
-  }, mounted() {
+  },
+  mounted() {
     var self = this;
     window.addEventListener('mousemove', function (event) {
       self.mousePos.x = event.clientX;
